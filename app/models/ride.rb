dca1d9df422eca940404b24c81208884231bcce1
alex <acontell@gmail.com>
2021-10-02 15:18:00 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,5 @@
 class Ride < ApplicationRecord
+    belongs_to :passenger, required: false
+    belongs_to :taxi, required: false
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts "Creating new rides..."
+30.times{Ride.create(taxi_id: rand(3), passenger_id:rand(10))}
+10.times{Passenger.create(name: Faker::Name.name)}
+3.times{Taxi.create(serial_number: Faker::Alphanumeric.alpha(number: 10))}
